c08d5ec7e81b0dc43f865f489bee5b9baaae05db
User <princeadexlonso@rocketmail.com>
2024-09-18 03:30:43 -0700

    add api and others

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myyelp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "myyelp5085c7ee": {
       "AppClientID": "string",
